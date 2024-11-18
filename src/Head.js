4d10570d0245f0583090e6a3a60690cf02824780
ManojPatil-0 <itsmanojmpatil@gmail.com>
2024-11-18 17:50:31 +0530

    login Button

--- src/Head.js
+++ src/Head.js
@@ -1,5 +1,7 @@
 import { MAIN_LOGO } from "../Utils/constant";
+import { useState } from "react";
 const Head = () =>{
+    const [btnname,setbtname] = useState('Login')
     return (
         <div className="head-container">
             <div className="logo">
@@ -11,6 +13,9 @@ const Head = () =>{
                     <ul>About Us</ul>
                     <ul>Contact Us</ul>
                     <ul>Cart</ul>
+                    <button className="btn-login" onClick={ () => {
+                        btnname === "Login" ? setbtname("Logout") : setbtname("Login");
+                    } }>{btnname}</button>
                 </li>
             </div>
         </div>
